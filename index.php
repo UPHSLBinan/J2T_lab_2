5b172a22c63554ef76ebef94a0f98c988f970405
ruzel <60469500+hemmy1103@users.noreply.github.com>
2023-02-10 19:39:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,18 @@ $password = "myadmin";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE Hernandez";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
 ?>
