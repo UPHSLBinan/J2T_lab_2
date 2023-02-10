ee10aba8eed8179fe54906273ab5007468a34619
moninaviardo <c18-1335-813@uphsl.edu.ph>
2023-02-10 19:54:38 +0800

    php database table done

--- index.php
+++ index.php
@@ -2,20 +2,27 @@
 $servername = "localhost";
 $username = "myadmin";
 $password = "myadmin";
+$dbname = "viardo";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Create database
-$sql = "CREATE DATABASE VIARDO";
+// sql to create table
+$sql = "CREATE TABLE Monina(
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
 if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
+  echo "Table Monina Vera Annecreated successfully";
 } else {
-  echo "Error creating database: " . $conn->error;
+  echo "Error creating table: " . $conn->error;
 }
 
 $conn->close();
