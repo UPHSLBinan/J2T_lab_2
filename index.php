7969b49b48090a4d00d1a323802947af1876d5a1
Cis <cishernandez10@gmail.com>
2023-02-10 19:41:32 +0800

    php database creation done

--- index.php
+++ index.php
@@ -5,10 +5,18 @@ $password = "Ranonline0110";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
-
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE Hernandez";
+if (mysqli_query($conn, $sql)) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
