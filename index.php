516418cfafff58c953c5f1da9e1bcd5870e6822e
ArmannnDs <maui.viernes@gmail.com>
2023-02-10 19:39:48 +0800

    I love you sir

--- index.php
+++ index.php
@@ -4,11 +4,19 @@ $username = "myadmin";
 $password = "myadmin";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password);
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE VIERNES";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
 ?>
