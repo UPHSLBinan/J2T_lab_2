779922440864186232acbbf4a6310b11236b8060
Cis <cishernandez10@gmail.com>
2023-02-10 19:47:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,28 @@
 $servername = "localhost";
 $username = "cishernandez";
 $password = "Ranonline0110";
+$dbname = "Hernandez";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-// Create database
-$sql = "CREATE DATABASE Hernandez";
-if (mysqli_query($conn, $sql)) {
-  echo "Database created successfully";
-} else {
-  echo "Error creating database: " . mysqli_error($conn);
-}
-
+// sql to create table
+$sql = "CREATE TABLE Francis (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+    
+    if (mysqli_query($conn, $sql)) {
+        echo "Table Francis created successfully";
+      } else {
+        echo "Error creating table: " . mysqli_error($conn);
+      }
 mysqli_close($conn);
 ?>
