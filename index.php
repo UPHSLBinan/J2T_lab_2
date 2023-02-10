f8ed6a5b9f956394ef77dc495d677ecead3c5c17
jhzz <Jhzzsmalig@gmail.com>
2023-02-10 19:51:44 +0800

    Added Table

--- index.php
+++ index.php
@@ -2,21 +2,28 @@
 $servername = "localhost";
 $username = "Malig";
 $password = "";
+$dbname = "JMalig";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Create database
-$sql = "CREATE DATABASE JMalig";
-if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
-} else {
-  echo "Error creating database: " . $conn->error;
-}
-
-$conn->close();
-?>
+$sql = "CREATE TABLE Jhzz (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )";
+  
+  if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+  } else {
+    echo "Error creating table: " . $conn->error;
+  }
+  
+  $conn->close();
+  ?>
