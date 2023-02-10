edbcc01d99a7f334fded1a99ac1012222ebcb654
ruzel <60469500+hemmy1103@users.noreply.github.com>
2023-02-10 19:56:46 +0800

    Created Database Table

--- index.php
+++ index.php
@@ -2,28 +2,20 @@
 $servername = "localhost";
 $username = "myadmin";
 $password = "myadmin";
-$dbname = "Hernandez";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
-$sql = "CREATE TABLE Ruzel (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
-
+// Create database
+$sql = "CREATE DATABASE Hernandez";
 if ($conn->query($sql) === TRUE) {
-  echo "Table Ruzel created successfully";
+  echo "Database created successfully";
 } else {
-  echo "Error creating table: " . $conn->error;
+  echo "Error creating database: " . $conn->error;
 }
 
 $conn->close();
