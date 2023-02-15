7011bac65ae0b1dc7d5112caf3fcc4a6db9f17c3
Stevenkurt <c21-1589-254@uphsl.edu.ph>
2023-02-15 19:42:12 +0800

    I love you sir

---
+++ index1.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="savedb.php" method="post">
+firstname: <input type="text" name="FNAME"><br>
+lastname: <input type="text" name="LNAME"><br>
+age: <input type="text" name="AGE"><br>
+E-mail: <input type="text" name="EMAIL"><br>
+detail: <input type="text" name="DETAIL"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
