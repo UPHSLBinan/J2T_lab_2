87322b673bb8e807d98e5d3798cfbf6e9b66b956
bdimaguila <briandimaguila1@gmail.com>
2023-02-15 19:51:08 +0800

    gitcommit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "myadmin";
+$password = "myadmin";
+$dbname = "dimaguila";
+
+$Fname = $_POST['Fname'];
+$Lname = $_POST['Lname'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+$detail = $_POST['detail'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO users (Fname, Lname, age, email, detail)
+VALUES ('".$Fname."','".$Lname."','".$age."','".$email."','".$detail."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
