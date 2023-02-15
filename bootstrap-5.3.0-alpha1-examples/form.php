4016688f7691976d0580e502935ca38d93d51e3c
Jose Mejarito <>
2023-02-15 19:12:44 +0800

    Inserting data and Handling done

---
+++ bootstrap-5.3.0-alpha1-examples/form.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="Index.php" method="post">
+First Name: <input type="text" name="fname"><br>
+Last Name: <input type="text" name="lname"><br>
+Age: <input type="text" name="age"><br>
+E-mail: <input type="text" name="email"><br>
+Detail: <input type="text" name="detail"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
