d7494906e462dc2837ec9f9256deb580f89fb3ab
Ruzel Hernandez <hemmybear@gmail.com>
2023-02-15 19:52:17 +0800

    user database hernandez

---
+++ databaseHernandez.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="databasehernandez1.php" method="POST">
+First Name:  <input type="varchar" name="Firstname"><br>
+Last Name: <input type="varchar" name="Lastname"><br>
+Age:        <input type="int"  name="Age"><br>
+Email:      <input type="varchar" name="Email"><br>
+Detail:     <input type="varchar" name="Detail"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
