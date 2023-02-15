f96839401bab43d868228983f3be5b6fcacdc215
Bon <c21-1372-869@uphsl.edu.ph>
2023-02-15 19:43:36 +0800

    user Database
    I love you sir ;*

---
+++ SAVEDATABASE.php
@@ -0,0 +1,30 @@
+<?php
+$FNAME = $_POST["FNAME"];
+$LNAME = $_POST["LNAME"];
+$AGE = $_POST["AGE"];
+$EMAIL = $_POST["EMAIL"];
+$DETAIL = $_POST["DETAIL"];
+
+$servername = "localhost";
+$username = "myadmin";
+$password = "myadmin";
+$dbname = "yap";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO users (FNAME, LNAME, AGE, EMAIL, DETAIL)
+VALUES ('".$FNAME."','".$LNAME."','".$AGE."','".$EMAIL."','".$DETAIL."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ index1.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="SAVEDATABASE.php" method="post">
+First Name: <input type="text" name="FNAME"><br>
+Last Name: <input type="text" name="LNAME"><br>
+Age: <input type="number" name="AGE"><br>
+Email: <input type="email" name="EMAIL"><br>
+Detail: <input type="text" name="DETAIL"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
