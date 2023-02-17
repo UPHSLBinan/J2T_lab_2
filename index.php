3bc6d8c41e326516b2e09a60c2e2b27766a78d66
Joshua Reyes <115640460+Ahzen619@users.noreply.github.com>
2023-02-17 18:51:51 +0800

    joshua reyes

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 $servername = "localhost";
 $username = "myadmin";
 $password = "myadmin";
-$dbname = "REYES";
+$dbname = "reyes";
+
+$Fname = $_POST['Fname'];
+$Lname = $_POST['Lname'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+$detail = $_POST['detail'];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,19 +17,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
-$sql = "CREATE TABLE REYES (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+$sql = "INSERT INTO users (Fname, Lname, age, email, detail)
+VALUES ('".$Fname."','".$Lname."','".$age."','".$email."','".$detail."')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "REYES created successfully";
+  echo "New record created successfully";
 } else {
-  echo "Error creating table: " . $conn->error;
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
