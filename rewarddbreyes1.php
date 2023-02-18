9b6bb7e35ed6e467ce6e7002c046a17ecbcdc933
Ahzen619 <115640460+Ahzen619@users.noreply.github.com>
2023-02-18 13:35:57 +0800

    I'm Done :)

---
+++ rewarddbreyes1.php
@@ -0,0 +1,27 @@
+<?php
+$Username = $_POST['Username'];
+$Password= $_POST['Password'];
+
+$servername = "localhost";
+$username = "myadmin";
+$password = "myadmin";
+$dbname = "reyes";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO login (Username, Password)
+VALUES ('".$Username."', '".$Password."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
