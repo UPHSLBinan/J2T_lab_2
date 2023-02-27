26807bd6e48063fc50a38aeab44be420b60f54e5
Cis <cishernandez10@gmail.com>
2023-02-27 17:46:50 +0800

    database table can be viewed in php file

--- login.php
+++ login.php
@@ -4,7 +4,12 @@ if (!isset($_SESSION['username']) || !isset($_SESSION['password'])) {
     // User is not logged in, redirect to the login page
     header('Location: index.php');
     exit;
-}?>
+}
+                  $servername = "localhost"; 
+                  $username = "hernandez"; 
+                  $password = "ranonline0110"; 
+                  $db = "hernandez"; 
+                  $conn = new mysqli($servername, $username, $password, $db);?>
 <html>
   <head>
     <title>Registration</title>
@@ -37,7 +42,51 @@ if (!isset($_SESSION['username']) || !isset($_SESSION['password'])) {
                
               </div>
               <div class="card-body">
-                asdasd
+                <table class ="table table-bordered table-striped">
+                  <thead>
+                    <tr>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Age</th>
+                      <th>Age</th>
+                      <th>Detail</th>
+                      <th>Action</th>
+                     </tr>
+                  </thead> 
+                <tbody>
+                  <?php
+                        $query = "SELECT * FROM francis1";
+                        $conn = mysqli_connect($servername, $username, $password, $db);
+                        $query_run = mysqli_query($conn,$query);
+
+                        if(mysqli_num_rows($query_run) > 0 )
+                        {
+                          foreach($query_run as $users)
+                          {
+                            ?>
+                            <tr>
+                              <td><?= $users['fname']; ?></td>
+                              <td><?= $users['lname']; ?></td>
+                              <td><?= $users['age']; ?></td>
+                              <td><?= $users['email']; ?></td>
+                              <td><?= $users['detail']; ?></td>
+                              <td>
+                                <a href="student-view.php?id=<?= $student['id']; ?>" class="btn btn-info btn-sm">View</a>
+                                <a href="student-edit.php?id=<?= $student['id']; ?>" class="btn btn-success btn-sm">Edit</a>
+                                <form action="code.php" method="POST" class="d-inline">
+                                    <button type="submit" name="delete_student"  class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                              </td>
+                          </tr>
+                          <?php
+                          }
+                        }
+                        else
+                                    {
+                                        echo "<h5> No Record Found </h5>";
+                                    }
+                                ?>
+
               </div>
               </div>
               </div>
