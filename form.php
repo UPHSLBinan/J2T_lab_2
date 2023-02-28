99e6ae6e59936708a1e1dc0f28ac7965fb9cb727
keiji27 <123733133+keiji27@users.noreply.github.com>
2023-02-28 13:28:27 +0800

    blog

--- form.php
+++ form.php
@@ -2,11 +2,13 @@
 <body>
 
 <form action="index.php" method="post">
-Firstname: <input type="text" name="Fname"><br>
-Lastname: <input type="text" name="Lname"><br>
-Age: <input type="text" name="Age"><br>
-E-mail: <input type="text" name="Email"><br>
-Detail: <input type="text" name="Detail"><br>
+Username: <input type="text" name="Fname"><br>
+Password: <input type="text" name="Lname"><br>
+First Name: <input type="text" name="Age"><br>
+Last Name: <input type="text" name="Email"><br>
+Age: <input type="text" name="Detail"><br>
+Birthday: <input type="text" name="Detail"><br>
+Address: <input type="text" name="Detail"><br>
 <input type="submit">
 </form>
 
